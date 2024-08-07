5f4a2e3996cdcc8754787979240128250486841c
IagorSs <iagor.penaranda@uol.com.br>
2024-08-06 21:30:24 -0300

    Add migrations

---
+++ migrations/2-insert_radares.sql
@@ -0,0 +1,124 @@
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (241, 70, 3271, -19.8307, -43.94143, 'Av. Cristiano Machado, nº 10.691', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (204, 60, 3279, -19.897, -43.94836, 'Av. Presidente Antônio Carlos, nº 1.694', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (225, 60, 3300, -19.914, -43.93656, 'Av. do Contorno, nº 629', 'Praça da Estação / Rodoviária');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (226, 60, 3301, -19.9295, -43.92189, 'Av. do Contorno, nº 4.045', 'Savassi / Sta. Efigênia');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (238, 60, 3306, -19.9272, -43.95275, 'Av. Amazonas, nº 2.153', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (236, 60, 3308, -19.9704, -43.99789, 'Av. Waldyr Soeiro Emrich, a 201 metros da Av. Joaquim de Oliveira', 'Betânia/Barreiro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (252, 60, 2411, -19.954112, -43.942929, 'Av. Nossa Senhora do Carmo, nº 1900', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (184, 60, 3255, -19.9281, -43.98292, 'Av. Teresa Cristina, a 27 metros da Rua Monte Simplon', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (198, 60, 3273, -19.86409, -43.93195, 'Av. Cristiano Machado, a 27 metros do Viaduto Paulo Mendes Campos', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (205, 60, 3280, -19.8307, -43.94143, 'Av. Presidente Antônio Carlos, oposto ao nº 8.281', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (242, 70, 3284, -19.8774, -43.92909, 'Av. Cristiano Machado, nº 9.950', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (232, 60, 3312, -19.8928, -43.96474, 'Av. Presidente Carlos Luz, nº 1.525', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (227, 60, 3302, -19.9186, -43.91217, 'Av. dos Andradas, oposto ao nº 3.470', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (254, 60, 2430, -19.92411, -43.98257, 'Av. Presidente Juscelino Kubitschek nº4333', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (183, 60, 3254, -19.917, -43.96494, 'Av. Teresa Cristina, nº 820', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (196, 60, 3267, -19.9383, -44.00037, 'Av. Amazonas oposto ao nº 7.800', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (203, 60, 3278, -19.9734, -44.01371, 'Av. Olinto Meireles, oposto ao nº 250', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (220, 70, 3292, -19.8191, -43.94702, 'Av. Cristiano Machado, nº 12.001 ', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (228, 60, 3303, -19.9102, -43.90911, 'Av. dos Andradas, nº 4.020', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (239, 60, 3305, -19.9308, -43.9632, 'Av. Amazonas, a 95 metros da rua São Carlos', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (194, 60, 3265, -19.9178, -43.94972, 'Av. do Contorno, nº 10.730', 'Barro Preto/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (207, 60, 3282, -19.9174, -43.96552, 'Av. Teresa Cristina, oposto ao nº 888', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (216, 60, 3288, -19.9234, -43.97769, 'Av. Teresa Cristina, nº 2.350', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (218, 70, 3290, -19.8774, -43.92909, 'Av. Cristiano Machado, esquina com a Rua Coronel Joaquim Tibúrcio', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (222, 60, 3295, -19.9303, -43.96205, 'Av. Cristiano Machado, nº 673', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (224, 60, 3299, -19.9288, -43.94125, 'Av. Bias Fortes, esquina com Rua Bernardo Guimarães', 'Pça. Liberdade / Pça. Raul Soares');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (231, 60, 3313, -19.8402, -43.96717, 'Av. Bernardo de Vasconcelos, oposto ao nº 1.720', 'União/Aparecida');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (187, 60, 3258, -19.8934, -43.92268, 'Av. José Cândido da Silveira, nº 509', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (189, 60, 3260, -19.8881, -43.95039, 'Av. Presidente Antônio Carlos, nº 2.764', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (201, 60, 3276, -19.864, -43.93149, 'Av. Cristiano Machado, a 89 metros da Rua Cajuí', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (202, 60, 3277, -19.9639, -43.95544, 'Av. Raja Gabaglia, esquina com Rua Yvon Magalhães Pinto', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (195, 60, 3266, -19.905, -43.93572, 'Av. Cristiano Machado, a 12 metros da rua Pitangui', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (234, 60, 3310, -19.9408, -43.95369, 'Av. Raja Gabaglia, oposto ao nº 617', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (197, 60, 3272, -19.9351, -43.99403, 'Av. Amazonas oposto ao nº 6.894', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (229, 60, 3316, -19.9597, -43.99912, 'Av. Teresa Cristina, a 22 metros da rua Carmelita Prates da Silva', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (249, 60, 3373, -19.8609, -43.96398, 'Av. Antônio Abrahão Caram, a 43 metros da Alameda das Acácias', 'São Luiz/Liberdade');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (209, 60, 3283, -19.9377, -43.92711, 'Av. Afonso Pena, esquina com Rua Maranhão', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (199, 60, 3274, -19.9302, -43.98067, 'Av. Amazonas, oposto ao nº 5.400', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (185, 60, 3256, -19.8888, -43.96733, 'Av. Presidente Carlos Luz, oposto ao nº 2.754', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (193, 60, 3264, -19.9176, -43.94896, 'Av. do Contorno, oposto ao nº 10812', 'Centro/Barro Preto');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (212, 60, 3269, -19.8743, -44.00002, 'Av. Heráclito Mourão de Miranda, nº 2.491', 'Pampulha/Alípio de Melo');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (210, 60, 3285, -19.9267, -43.95242, 'Av. Amazonas, nº 2.086', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (206, 60, 3281, -19.8491, -43.91955, 'Av. Risoleta Neves, a 138m da Rua Raimunda Ferreira da Rocha', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (188, 60, 3259, -19.8874, -43.95081, 'Av. Presidente Antônio Carlos, oposto ao nº 2.862', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (253, 60, 3262, -19.95539, -43.94042, 'Av. Nossa Senhora do Carmo, nº 1.925', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (217, 60, 3289, -19.9275, -43.98305, 'Av. Teresa Cristina, nº 3.826', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (233, 60, 3311, -19.9016, -43.99075, 'Av. Presidente Tancredo Neves, a 116 metros da Rua Flor Branca', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (208, 60, 3315, -19.8548, -43.92286, 'Av. Risoleta Neves, a 285 metros da Rua da Pedreira', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (223, 60, 3296, -19.9635, -43.96328, 'Av. Barão Homem de Melo, oposto ao nº 3.191', 'Av. Silva Lobo / Av. Raja Gabáglia');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (237, 60, 3307, -19.9821, -44.00658, 'Av. Waldyr Soeiro Emrich, a 68 metros da Rua dos Americanos', 'Betânia/Barreiro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (213, 60, 3268, -19.9391, -43.92815, 'Av. do Contorno, oposto ao nº 5.445', 'São Lucas/Savassi');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (251, 60, 22, -19.9002, -43.99704, 'Av. João XXIII, oposto ao nº 1.233', 'Av. Presidente Tancredo Neves');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (235, 60, 3309, -19.9597, -43.99912, 'Av. Teresa Cristina, a 25 metros da rua Carmo da Cachoeira', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (186, 60, 3257, -19.9711, -43.95118, 'Av. Raja Gabaglia, nº 4.847', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (214, 60, 3286, -19.8497, -43.96566, 'Av. Presidente Antônio Carlos, nº 8.271', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (190, 60, 3261, -19.9014, -43.94577, 'Av. Presidente Antônio Carlos, nº 1089', 'Bairro/Centro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (211, 60, 3270, -19.8842, -43.91606, 'Av. José Cândido da Silveira, nº 1.500', 'Centro/Bairro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (230, 60, 3314, -19.9771, -44.005, 'Av. Waldyr Soeiro Emrich, a 118 metros da Rua Alfredina Amaral ', 'Betânia/Barreiro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (250, 60, 23, -19.90086, -43.99492, 'Av. João XXIII, nº 1.233', 'Av. Abílio Machado');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (246, 60, 3318, -19.9734, -44.00578, 'Av. Deputado álvaro Antônio, a 280 metros da Av. Adão Alves de Souza', 'Betânia/Barreiro');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (248, 60, 3372, -19.9649, -43.99596, 'Av. Eliseu Resende, esquina com Rua Lotus', 'Rua Ursula Paulino/Av. Teresa Cristina');
+INSERT INTO radares (id_eqp, velocidade_permitida, numero_serie, latitude, longitude, endereco, sentido)
+  VALUES (182, 60, 3253, -19.9918, -44.01711, 'Av. Waldyr Soeiro Emrich, a 45 metros da Rua Aladin Correa de Faria', 'Jatobá/Milionários');
